3d6632cdd6935a02dd8df7e08077180ba2d95b0e
luokeqiang <luoke9681@163.com>
2022-06-07 16:54:43 +0800

    新增 QuickUnion 算法

---
+++ src/main/java/org/luoke/disjointset/QuickUnion.java
@@ -0,0 +1,5 @@
+package org.luoke.disjointset;/**
+* @author luoke
+* @date 2022/6/7 16:21
+*/public class QuickUnion {
+}
---
+++ src/test/java/org/luoke/disjointset/QuickUnionTest.java
@@ -0,0 +1,7 @@
+import static org.junit.jupiter.api.Assertions.*;
+/**
+* @author luoke
+* @date 2022/6/7 16:28
+*/class QuickUnionTest {
+  
+}
