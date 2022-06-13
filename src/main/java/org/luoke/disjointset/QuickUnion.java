8c0a9209b7b463bc73776f13f0abf34de47cd0fc
luokeqiang <luoke9681@163.com>
2022-06-13 09:32:33 +0800

    add UnionFind

--- src/main/java/org/luoke/disjointset/QuickUnion.java
+++ src/main/java/org/luoke/disjointset/QuickUnion.java
@@ -1,5 +1,32 @@
-package org.luoke.disjointset;/**
-* @author luoke
-* @date 2022/6/7 16:21
-*/public class QuickUnion {
+package org.luoke.disjointset;
+
+/**
+ * @author luoke
+ * @date 2022/6/7 16:21
+ */
+public class QuickUnion {
+    private final int[] data;
+    public QuickUnion(int capacity){
+        data = new int[capacity];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        int root = x;
+        while (root != data[root]) {
+            root = data[root];
+        }
+        while (root != x) {
+            x = data[x];
+            data[x] = root;
+        }
+        return root;
+    }
+
+    public void merge(int x, int y) {
+        data[find(x)] = find(y);
+    }
+
 }
--- src/test/java/org/luoke/disjointset/QuickUnionTest.java
+++ src/test/java/org/luoke/disjointset/QuickUnionTest.java
@@ -1,7 +1,42 @@
-import static org.junit.jupiter.api.Assertions.*;
-/**
-* @author luoke
-* @date 2022/6/7 16:28
-*/class QuickUnionTest {
-  
-}
+//package org.luoke.disjointset;
+//
+//import org.junit.jupiter.api.*;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+///**
+// * @author luoke
+// * @date 2022/6/7 16:28
+// */
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//class QuickUnionTest {
+//    static QuickUnion quickUnion;
+//    @BeforeAll
+//    static void init() {
+//        quickUnion = new QuickUnion(5);
+//    }
+//
+//    @Test
+//    @Order(2)
+//    void find() {
+//        assertEquals(2, quickUnion.find(2));
+//        assertEquals(2, quickUnion.find(4));
+//        assertEquals(2, quickUnion.findWithIterator(2));
+//        assertEquals(2, quickUnion.findWithIterator(4));
+//        assertEquals(4, quickUnion.getSize(2));
+//        assertEquals(2, quickUnion.getSize(1));
+//    }
+//
+//    @Test
+//    @Order(1)
+//    void merge() {
+//        quickUnion.merge(2, 4);
+//        quickUnion.merge(1,3);
+//        quickUnion.merge(2,1);
+//    }
+//
+//    @Test
+//    void test(){
+//
+//    }
+//}
